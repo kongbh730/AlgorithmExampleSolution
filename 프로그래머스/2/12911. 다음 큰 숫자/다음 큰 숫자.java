2e794649c21a338f154e50d03998dc96e7f77b4b
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-01-21 22:02:32 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,34 +1,17 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String binaryString = Integer.toBinaryString(n);
-        int count = 0, temp;
-        int i, j;
+        int count = 0, temp = 0;
         
-        //System.out.println(binaryString);
-        //이렇게 하면 시간이 너무 오래 걸릴거 같은데....
-        for(i = 0; i < binaryString.length(); i++){
-            if(binaryString.charAt(i) == '1'){
-                count++;
-            }
-        }
+        count = Integer.bitCount(n);
+        //System.out.println(n + "의 비트 수 : " + count);
         
-        while(true){
+        while(temp != count){
             n++;
-            String tempBinaryString = Integer.toBinaryString(n);
-            temp = 0;
-            
-            //System.out.println(n + " " + tempBinaryString);
-            for(i = 0; i < tempBinaryString.length(); i++){
-                if(tempBinaryString.charAt(i) == '1'){
-                    temp++;
-                }
-            }
-            if(temp == count){
-                answer = n;
-                break;
-            }
+            temp = Integer.bitCount(n);
+            //System.out.println(n + "의 비트 수 : " + temp);
         }
+        answer = n;
         
         return answer;
     }
