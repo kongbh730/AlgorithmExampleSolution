ff79a7af676a5c5a8817774c3044416de99b4bb3
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-03-07 13:07:12 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 작성해주세요
+
+-- 아이템의 희귀도가 'RARE'인 아이템들의
+-- 모든 다음 업그레이드 아이템의 
+-- 아이템 ID(ITEM_ID), 아이템 명(ITEM_NAME), 아이템의 희귀도(RARITY)를 출력하는 SQL 문을 작성해 주세요. 
+-- 이때 결과는 아이템 ID를 기준으로 내림차순 정렬주세요.
+
+select ITEM_INFO.ITEM_ID, ITEM_INFO.ITEM_NAME, ITEM_INFO.RARITY
+from ITEM_INFO 
+where ITEM_INFO.ITEM_ID IN 
+(
+    SELECT ITEM_TREE.ITEM_ID
+    FROM ITEM_INFO, ITEM_TREE
+    WHERE ITEM_INFO.ITEM_ID = ITEM_TREE.PARENT_ITEM_ID AND ITEM_INFO.RARITY = 'RARE'
+)
+order by ITEM_INFO.ITEM_ID desc;
