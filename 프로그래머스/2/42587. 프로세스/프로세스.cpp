eabfc37b1e337504e4279371edf6a70ae4164923
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-03-10 16:32:23 +0900

    [level 2] Title: 프로세스, Time: 0.28 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -18,19 +18,21 @@ int solution(vector<int> priorities, int location) {
     
     while(!q.empty()){
         if(q.front().second == pq.top() && q.front().first == location){//찾고나 하는 인덱스를 찾은 경우
+            cout << q.front().first << "의 우선순위 : " << pq.top()<< "\n";
             answer++;
             break;
         }
         else if(q.front().second == pq.top()){
+            cout << "큐 제거 : " << q.front().first << "의 우선순위 : " << pq.top()<< "\n";
             pq.pop();
             q.pop();
             answer++;
-            cout << "큐 제거\n";
         }
         else{
+            cout << "현재 pq의 우선순위 : " << pq.top() << "\n";
+            cout << "큐 뒤에 추가 : " << q.front().first << "의 우선순위 : " << q.front().second << "\n";
             q.push(q.front());
             q.pop();
-            cout << "큐 뒤에 추가\n";
         }
     }
     
