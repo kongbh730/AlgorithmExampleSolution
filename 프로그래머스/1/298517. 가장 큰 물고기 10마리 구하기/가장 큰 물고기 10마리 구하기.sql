0aab219c826341cd47d6a11c8266831697ffdd93
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-03-11 10:06:48 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -0,0 +1,14 @@
+-- 코드를 작성해주세요
+
+-- FISH_INFO 테이블에서 from FISH_INFO
+-- 가장 큰 물고기 10마리의 ID와 길이를 출력하는 SQL 문을 작성해주세요. select ID, LENGTH
+-- 결과는 길이를 기준으로 내림차순 정렬하고, order by LENGTH DESC
+-- 길이가 같다면 물고기의 ID에 대해 오름차순 정렬해주세요.  order by ID ASC
+-- 단, 가장 큰 물고기 10마리 중 길이가 10cm 이하인 경우는 없습니다. 
+-- ID 컬럼명은 ID, 길이 컬럼명은 LENGTH로 해주세요.
+
+select ID, LENGTH
+from FISH_INFO
+where LENGTH > 10
+order by LENGTH desc, ID asc
+limit 10;
