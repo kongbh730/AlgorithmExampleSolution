2903010ed745824c87f5ba3dd086ba368dc80a87
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-08-26 11:00:59 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,13 +1,19 @@
--- 코드를 입력하세요
+# -- 코드를 입력하세요
 
--- ONLINE_SALE 테이블에서 from ONLINE_SALE
--- 동일한 회원이 동일한 상품을 재구매한 데이터를 구하여, 
--- 재구매한 회원 ID와 재구매한 상품 ID를 출력하는 SQL문을 작성해주세요. select USER_ID, PRODUCT_ID
--- 결과는 회원 ID를 기준으로 오름차순 정렬해주시고 order by USER_ID asc
--- 회원 ID가 같다면 상품 ID를 기준으로 내림차순 정렬해주세요. order by PRODUCT_ID desc
+# -- ONLINE_SALE 테이블에서 from ONLINE_SALE
+# -- 동일한 회원이 동일한 상품을 재구매한 데이터를 구하여, group by USER_ID, PRODUCT_ID having count(*) > 1
+# -- 재구매한 회원 ID와 재구매한 상품 ID를 출력하는 SQL문을 작성해주세요. select USER_ID, PRODUCT_ID
+# -- 결과는 회원 ID를 기준으로 오름차순 정렬해주시고 order by USER_ID asc
+# -- 회원 ID가 같다면 상품 ID를 기준으로 내림차순 정렬해주세요. order by PRODUCT_ID desc
+
+# SELECT USER_ID, PRODUCT_ID, count(*)
+# from ONLINE_SALE
+# group by USER_ID, PRODUCT_ID
+# order by USER_ID asc, PRODUCT_ID desc;
 
 SELECT USER_ID, PRODUCT_ID
-from ONLINE_SALE
-group by USER_ID, PRODUCT_ID
-having count(*) > 1
-order by USER_ID asc, PRODUCT_ID desc;
+FROM ONLINE_SALE 
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) >= 2
+ORDER BY USER_ID ASC, PRODUCT_ID DESC;
+
