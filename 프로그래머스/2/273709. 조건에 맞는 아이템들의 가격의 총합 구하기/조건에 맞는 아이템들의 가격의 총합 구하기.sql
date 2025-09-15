2c43957af177353d349ae32f6041fb6f5badb8f1
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-09-15 10:31:28 +0900

    [level 2] Title: 조건에 맞는 아이템들의 가격의 총합 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273709. 조건에 맞는 아이템들의 가격의 총합 구하기/조건에 맞는 아이템들의 가격의 총합 구하기.sql
+++ 프로그래머스/2/273709. 조건에 맞는 아이템들의 가격의 총합 구하기/조건에 맞는 아이템들의 가격의 총합 구하기.sql
@@ -4,6 +4,11 @@
 -- 가격의 총합을 구하는 SQL문을 작성해 주세요.  select sum(price)
 -- 이때 컬럼명은 'TOTAL_PRICE'로 지정해 주세요. as TOTAL_PRICE
 
-select sum(price) as TOTAL_PRICE
-from ITEM_INFO
-where RARITY = 'LEGEND';
+# select sum(price) as TOTAL_PRICE
+# from ITEM_INFO
+# where RARITY = 'LEGEND';
+
+
+SELECT SUM(PRICE) AS TOTAL_PRICE
+FROM ITEM_INFO
+WHERE RARITY = 'LEGEND'
