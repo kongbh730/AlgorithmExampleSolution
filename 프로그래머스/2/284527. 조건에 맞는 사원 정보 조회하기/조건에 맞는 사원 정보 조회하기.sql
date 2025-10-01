12303f9719dee8a8b78442a611113cc6672fb3bb
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-10-01 16:26:33 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -2,8 +2,15 @@
 -- 2022년도 한해 평가 점수가 가장 높은 사원 정보를 조회하려 합니다. 2022년도 평가 점수가 가장 높은 사원들의 점수, 사번, 성명, 직책, 이메일을 조회하는 SQL문을 작성해주세요.
 -- 2022년도의 평가 점수는 상,하반기 점수의 합을 의미하고, 평가 점수를 나타내는 컬럼의 이름은 SCORE로 해주세요.
 
-select sum(HG.SCORE) as SCORE, HG.EMP_NO, HE.EMP_NAME, HE.POSITION, HE.EMAIL
-from HR_GRADE HG join HR_EMPLOYEES HE on HG.EMP_NO = HE.EMP_NO
-group by HG.EMP_NO
-order by sum(HG.SCORE) desc
-limit 1;
+# select sum(HG.SCORE) as SCORE, HG.EMP_NO, HE.EMP_NAME, HE.POSITION, HE.EMAIL
+# from HR_GRADE HG join HR_EMPLOYEES HE on HG.EMP_NO = HE.EMP_NO
+# where HG.YEAR = 2022
+# group by HG.EMP_NO
+# order by sum(HG.SCORE) desc
+# limit 1;
+
+SELECT SUM(HG.SCORE) AS SCORE, HG.EMP_NO, HE.EMP_NAME, HE.POSITION, HE.EMAIL
+FROM HR_GRADE HG JOIN  HR_EMPLOYEES HE ON HG.EMP_NO = HE.EMP_NO
+GROUP BY HG.EMP_NO
+ORDER BY SUM(HG.SCORE) DESC
+LIMIT 1
