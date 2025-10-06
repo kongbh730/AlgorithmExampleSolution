ebacedc50751fca11d645034b1080ed493bddd58
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-10-06 20:42:38 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -4,9 +4,25 @@
 -- 물고기의 이름 컬럼명은 FISH_NAME, 잡은 수 컬럼명은 FISH_COUNT로 해주세요.
 -- 결과는 잡은 수 기준으로 내림차순 정렬해주세요.
 
+# select count(*) as FISH_COUNT, 
+#     (
+#         select FISH_NAME 
+#         from FISH_NAME_INFO 
+#         where FISH_NAME_INFO.FISH_TYPE = FISH_INFO.FISH_TYPE
+#     ) as FISH_NAME
+# from FISH_INFO 
+# group by FISH_TYPE
+# order by FISH_COUNT desc;
 
-select count(*) as FISH_COUNT, 
-    (select FISH_NAME from FISH_NAME_INFO where FISH_NAME_INFO.FISH_TYPE = FISH_INFO.FISH_TYPE) as FISH_NAME
-from FISH_INFO 
-group by FISH_TYPE
-order by FISH_COUNT desc;
+# select 
+#     COUNT(FI.FISH_TYPE) AS FISH_COUNT,
+#     FNI.FISH_NAME AS FISH_NAME
+# from FISH_INFO FI
+# join FISH_NAME_INFO FNI on FI.FISH_TYPE = FNI.FISH_TYPE
+# group by FNI.FISH_NAME
+# order by FISH_COUNT desc;
+
+SELECT COUNT(FI.FISH_TYPE) AS FISH_COUNT, FNI.FISH_NAME
+FROM FISH_INFO FI JOIN FISH_NAME_INFO FNI ON FI.FISH_TYPE = FNI.FISH_TYPE
+GROUP BY FNI.FISH_NAME
+ORDER BY FISH_COUNT DESC
