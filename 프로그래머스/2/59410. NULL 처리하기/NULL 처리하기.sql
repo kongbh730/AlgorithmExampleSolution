07c0b99f83951876673d55dd9cdbd2d3fb16da12
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-10-08 13:46:15 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -4,6 +4,10 @@
 -- 아이디 순으로 조회하는 SQL문을 작성해주세요. order by ANIMAL_ID
 -- 이때 프로그래밍을 모르는 사람들은 NULL이라는 기호를 모르기 때문에, 이름이 없는 동물의 이름은 "No name"으로 표시해 주세요. select ifnull(NAME, 'No name')
 
-SELECT ANIMAL_TYPE, ifnull(NAME, 'No name') as NAME, SEX_UPON_INTAKE
-from ANIMAL_INS 
-order by ANIMAL_ID;
+# SELECT ANIMAL_TYPE, ifnull(NAME, 'No name') as NAME, SEX_UPON_INTAKE
+# from ANIMAL_INS 
+# order by ANIMAL_ID;
+
+SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name'), SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
