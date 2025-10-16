b0857b292182aa78817847798d4bc9ea73b8cd19
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-10-16 09:41:31 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -4,15 +4,24 @@
 -- 거래상태가 SALE 이면 판매중, RESERVED이면 예약중, DONE이면 거래완료 분류하여 출력해주시고, 
 -- 결과는 게시글 ID를 기준으로 내림차순 정렬해주세요.
 
-SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, case 
-    when STATUS = 'SALE'
-    then '판매중'
-    when STATUS = 'RESERVED'
-    then '예약중'
-    when STATUS = 'DONE'
-    then '거래완료'
-    end as STATUS
-from USED_GOODS_BOARD
-where CREATED_DATE = '2022-10-05'
-order by BOARD_ID desc;
+# SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, case 
+#     when STATUS = 'SALE'
+#     then '판매중'
+#     when STATUS = 'RESERVED'
+#     then '예약중'
+#     when STATUS = 'DONE'
+#     then '거래완료'
+#     end as STATUS
+# from USED_GOODS_BOARD
+# where CREATED_DATE = '2022-10-05'
+# order by BOARD_ID desc;
 
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+CASE
+    WHEN STATUS = 'SALE' THEN '판매중'
+    WHEN STATUS = 'RESERVED' THEN '예약중'
+    WHEN STATUS = 'DONE' THEN '거래완료'
+    END
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY BOARD_ID DESC;
