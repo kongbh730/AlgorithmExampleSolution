94a10d94efab199adf060fe6d88a265857541c2a
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2025-11-17 10:31:38 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,10 +1,17 @@
--- 2022년 1월의 카테고리 별 도서 판매량을 합산하고, 
--- 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력하는 SQL문을 작성해주세요.
--- 결과는 카테고리명을 기준으로 오름차순 정렬해주세요.
-SELECT BOOK.CATEGORY, sum(BOOK_SALES.SALES) as TOTAL_SALES
-from BOOK_SALES join BOOK on BOOK_SALES.BOOK_ID = BOOK.BOOK_ID
--- where year(BOOK_SALES.SALES_DATE) = 2022 and month(BOOK_SALES.SALES_DATE) = 1
--- where date_format(BOOK_SALES.SALES_DATE, '%Y-%m') = '2022-01'
-where BOOK_SALES.SALES_DATE between '2022-01-01' and '2022-01-31'
-group by CATEGORY
-order by CATEGORY asc;
+# -- 2022년 1월의 카테고리 별 도서 판매량을 합산하고, 
+# -- 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력하는 SQL문을 작성해주세요.
+# -- 결과는 카테고리명을 기준으로 오름차순 정렬해주세요.
+# SELECT BOOK.CATEGORY, sum(BOOK_SALES.SALES) as TOTAL_SALES
+# from BOOK_SALES join BOOK on BOOK_SALES.BOOK_ID = BOOK.BOOK_ID
+# -- where year(BOOK_SALES.SALES_DATE) = 2022 and month(BOOK_SALES.SALES_DATE) = 1
+# -- where date_format(BOOK_SALES.SALES_DATE, '%Y-%m') = '2022-01'
+# where BOOK_SALES.SALES_DATE >= '2022-01-01' and BOOK_SALES.SALES_DATE < '2022-02-01'
+# -- where BOOK_SALES.SALES_DATE between '2022-01-01' and '2022-01-31'
+# group by CATEGORY
+# order by CATEGORY asc;
+
+SELECT B.CATEGORY, SUM(BS.SALES) AS TOTAL_SALES
+FROM BOOK B JOIN BOOK_SALES BS ON B.BOOK_ID = BS.BOOK_ID
+WHERE BS.SALES_DATE BETWEEN "2022-01-01" AND "2022-01-31"
+GROUP BY B.CATEGORY
+ORDER BY B.CATEGORY ASC;
