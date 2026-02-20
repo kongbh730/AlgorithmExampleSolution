ec9f7398df53fe65a5aab1be647dc8ae4557693c
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2026-02-20 20:31:10 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(void){
+
+int N;
+
+cin >> N;
+if(N == 0){
+	cout << "YONSEI\n";
+}
+else{
+	cout << "Leading the Way to the Future\n";
+}
+
+return 0;
+}
