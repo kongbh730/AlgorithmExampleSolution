23190b873c40fb433f46b678490c64aef05efb8e
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2026-04-03 09:36:10 +0900

    [Bronze III] Title: ZOAC 4, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -1,67 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
+
     static class Table {
-        int H, W, N, M, maxCount;
+        private int height;
+        private int width;
+        private int rowGap;
+        private int colGap;
 
-        public int getH() {
-            return H;
+        public Table(int height, int width, int rowGap, int colGap) {
+            this.height = height;
+            this.width = width;
+            this.rowGap = rowGap;
+            this.colGap = colGap;
         }
 
-        public void setH(int h) {
-            H = h;
-        }
-
-        public int getW() {
-            return W;
-        }
-
-        public void setW(int w) {
-            W = w;
-        }
-
-        public int getN() {
-            return N;
-        }
-
-        public void setN(int n) {
-            N = n;
-        }
-
-        public int getM() {
-            return M;
-        }
-
-        public void setM(int m) {
-            M = m;
-        }
-
-        public void findAnswer(){
-            int i, j;
-
-            for(i = 0; i < H; i += (N+1)){
-                for(j = 0; j < W; j += (M+1)){
-                    maxCount++;
-                }
-            }
-        }
-
-        public int getMaxCount(){
-            return maxCount;
+        public int calculateMaxCount() {
+            int rowCount = (height + rowGap) / (rowGap + 1);
+            int colCount = (width + colGap) / (colGap + 1);
+            
+            return rowCount * colCount;
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int H, W, N, M;
 
-        Table table = new Table();
-        table.setH(sc.nextInt());
-        table.setW(sc.nextInt());
-        table.setN(sc.nextInt());
-        table.setM(sc.nextInt());
-        
-        table.findAnswer();
-        System.out.println(table.getMaxCount());
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        Table table = new Table(h, w, n, m);
+        System.out.println(table.calculateMaxCount());
+
+        sc.close();
     }
 }
